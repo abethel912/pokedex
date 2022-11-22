7406868a4d7ab56b89fb8f426bee9580d79de1cd
abethel912 <abethel180@gmail.com>
2022-11-21 20:07:55 -0500

    CSS linked

--- server.js
+++ server.js
@@ -2,21 +2,19 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 const Pokemon = require('./models/pokemon.js')
-
+const data = require('./models/pokemon.js')
+const methodOverride = require('method-override')
 
 // Allows you to use data from the URL
 // URLencoded - is the data in the URL
 // Need this to use the request.params and request.query
 app.use(express.urlencoded({ extended: true }))
-
-// Landing - GET /
-// app.get("/", (req, res) => {
-//   res.render("landing.ejs")
-// })
+// To use the public folder, with /public in the URL we need to use the express.static middleware
+app.use("/public", express.static("public"))
 
 // INDEX
 app.get('/pokemon', (req, res) => {
-  console.log(Pokemon.every(p => p.stats.hp))
+  
 res.render('index.ejs', { data: Pokemon });
 });
 
@@ -25,12 +23,10 @@ app.get('/pokemon/new', (req, res) => {
   res.render('new.ejs')
 })
 
-
-// Create-POST /pokemon
-
-
-
 // Destroy - DELETE /pokemon/:id
+app.delete("/pokemon/:id", (req, res) => {
+  res.render("new.ejs")
+})
 
 // SHOW // GET /pokemon/:id
 app.get('/pokemon/:id', (req, res) => {
@@ -39,10 +35,27 @@ app.get('/pokemon/:id', (req, res) => {
   })
 })
 // Edit - GET /pokemon/:id/edit
+app.put("/pokemon/:id", (req, res) => {
+  res.render(edit.ejs, {
+    data: Pokemon[req.params.id],
+    index:req.params.id,
+  })
+})
 
-
-
+// Create-POST /pokemon
+app.post("/pokemon", (req, res) => {
+  // req.body.type = [req.body.type]
+  data.push(req.body),
+  res.redirect("/pokemon")
+})
 //Update - PUT /pokemon/:id
+
+app.put('/pokemon/:id', (req, res) => {
+  data.update(req.params.id, req.body)
+  res.redirect("/pokemon")
+})
+
+// Listener
 
 const PORT = process.env.PORT || 3011
 app.listen(PORT, () => {
